30fa050fba5e69ad835b3370d11ecef439067e22
Nagarjun Rajendran <nagarjun.rajendran@bosch.com>
2026-01-03 23:09:40 -0600

    Working terraform deployment of twin in AWS

--- week2/workspace/twin/frontend/components/twin.tsx
+++ week2/workspace/twin/frontend/components/twin.tsx
@@ -41,7 +41,7 @@ export default function Twin() {
 
         try {
             // const response = await fetch('http://localhost:8000/chat', {
-            const response = await fetch('https://dnm0cc4ukk.execute-api.us-east-1.amazonaws.com/chat', {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/chat`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
